2d6a74425073c6a72c081944f7c50a78e140482c
Giovanni Giuseppe <77415161+giogiuseppe@users.noreply.github.com>
2024-11-10 23:24:13 +0000

    Trabalho Final - APIs

--- assets/data/0-schema.sql
+++ assets/data/0-schema.sql
@@ -1,7 +1,29 @@
 USE application;
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-)
+CREATE TABLE usuarios (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome_de_usuario VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE colaboradores (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tarefas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    data_criacao DATE,
+    data_inicio DATE,
+    data_conclusao DATE
+);
+
+CREATE TABLE tarefa_colaborador (
+    tarefa_id BIGINT NOT NULL,
+    colaborador_id BIGINT NOT NULL,
+    PRIMARY KEY (tarefa_id, colaborador_id),
+    FOREIGN KEY (tarefa_id) REFERENCES tarefas(id) ON DELETE CASCADE,
+    FOREIGN KEY (colaborador_id) REFERENCES colaboradores(id) ON DELETE CASCADE
+);
